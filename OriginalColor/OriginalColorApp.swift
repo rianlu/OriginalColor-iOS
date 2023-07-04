de73acc98f4146cfa4d21c604153b1924052e5d9
lu <2469480096@qq.com>
2023-07-04 13:30:17 +0800

    Initial Commit

---
+++ OriginalColor/OriginalColorApp.swift
@@ -0,0 +1,17 @@
+//
+//  OriginalColorApp.swift
+//  OriginalColor
+//
+//  Created by Lu on 2023/7/4.
+//
+
+import SwiftUI
+
+@main
+struct OriginalColorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
